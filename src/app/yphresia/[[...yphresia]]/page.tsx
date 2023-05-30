7152f98620e76d927cfa89beb69690cfde665e90
giorgos_gr <giorgos@webdimension.gr>
2023-05-30 14:04:21 +0300

    meta uphresia page

--- src/app/yphresia/[[...yphresia]]/page.tsx
+++ src/app/yphresia/[[...yphresia]]/page.tsx
@@ -14,6 +14,7 @@ async function getServiceData(service) {
             "Accept": "application/json",
             "Content-Type": "application/json",
         },
+        next: { revalidate: 1 },
         body: JSON.stringify({ path: path })
     })
 
@@ -47,7 +48,7 @@ async function getServiceData(service) {
 
 }
 
-async function getAllServicesData () {
+async function getAllServicesData() {
     const url = `${process.env.NEXT_PUBLIC_DRUPAL_URL}/jsonapi/node/service/?fields[node--service]=drupal_internal__nid,title,path`
 
     const res = await fetch(url,
@@ -73,7 +74,45 @@ async function getAllServicesData () {
 }
 
 
-const YphresiaPage = async ({ params: {yphresia} }) => {
+export async function generateMetadata({ params: { yphresia } }) {
+
+    const data_props = await getServiceData(yphresia)
+
+
+    const title = data_props?.data?.[0]?.attributes?.title
+    let string = data_props?.data[0]?.attributes?.body?.value.replace(/<(.|\n)*?>/g, "").trim().substring(0, 150)
+    const body = string.substring(0, Math.min(string.length, string.lastIndexOf(" ")));
+    const canonical = `yphresia/${yphresia.join('/')}`
+    const main_img = `${process.env.NEXT_PUBLIC_DRUPAL_URL}${data_props?.data?.[0]?.relationships?.field_image?.data?.[0]?.attributes?.uri?.url}`
+
+
+    return {
+        title: `${`${title} | ${process.env.NEXT_PUBLIC_SITE_NAME}`}`,
+        description: body,
+        alternates: {
+            canonical: `${process.env.NEXT_PUBLIC_NEXT_URL}/${canonical}`,
+        },
+        openGraph: {
+            title: `${`${title} | ${process.env.NEXT_PUBLIC_SITE_NAME}`}`,
+            description: body,
+            url: `${process.env.NEXT_PUBLIC_NEXT_URL}/${canonical}`,
+            siteName: `${process.env.NEXT_PUBLIC_SITE_NAME}`,
+            images: [
+                {
+                    url: main_img,
+                    width: 800,
+                    height: 600,
+                    alt: title,
+                },
+            ],
+            type: 'website',
+        },
+    }
+}
+
+
+
+const YphresiaPage = async ({ params: { yphresia } }) => {
 
     const data_promie = getServiceData(yphresia)
     const all_services_promise = getAllServicesData()
@@ -83,7 +122,7 @@ const YphresiaPage = async ({ params: {yphresia} }) => {
 
     return (
         <>
-            <YphresiaPageClient 
+            <YphresiaPageClient
                 service_props={data_props}
                 all_services_props={all_servive_props}
             />
