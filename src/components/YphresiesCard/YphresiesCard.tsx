91a679f93637de9f9831380aee29badd8e5597a4
giorgos_gr <giorgos@webdimension.gr>
2023-06-05 14:53:18 +0300

    add efe to yphresiaCard image

--- src/components/YphresiesCard/YphresiesCard.tsx
+++ src/components/YphresiesCard/YphresiesCard.tsx
@@ -18,6 +18,7 @@ const YphresiesCard = ({ title, img, body, icon }) => {
                             style={{ objectFit: "cover", top: '0', left: '0', right: '0', bottom: '0' }}
                             fill={true}
                         />
+                        <div className={styles.onhoverLayout}/>
                     </div>
                     <div className={`d-flex justify-content-center`}>
 
@@ -47,7 +48,7 @@ const YphresiesCard = ({ title, img, body, icon }) => {
                     <div className={`text-muted ${styles.body}`} style={title.length > 20 ? { ['--lines-to-show' as string]: '2' } : {}} dangerouslySetInnerHTML={{ __html: body }} />
 
                     <WdLink href='/'>
-                        <div className={`${styles.arrowLink}`}>
+                        <div className={`${styles.arrowLink} mt-3`}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                             </svg>
